2770747a4d1c8f6a932c8e60ff3662a206afc0d4
sweetmilkys <sweetmilky.dev@gmail.com>
2019-04-09 22:28:20 +0900

    Icon 사용하기

--- Weather.js
+++ Weather.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo";
+import { Ionicons } from "@expo/vector-icons";
 
 export default class Weather extends Component {
   render() {
     return (
       <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
         <View style={styles.upper}>
-          <Text>Icon</Text>
+          <Ionicons color="white" size={144} name="ios-rainy" />
           <Text style={styles.temp}>Tempº</Text>
         </View>
         <View style={styles.lower}>
